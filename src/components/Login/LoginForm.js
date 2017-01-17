309e71d98ecb302f41141e5842cd9061c8a309e1
Edwin Bosire <me@edwinb.co.uk>
2017-01-17 23:01:52 +0000

    removed autocorrect and capitalise keyboard functions

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TextInput, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, View, TextInput, TouchableOpacity, Text, StatusBar } from 'react-native';
  
 export default class LoginForm extends Component { 
     render() {
         return (
             <View style={ styles.container }>
+            <StatusBar barStyle="default" />
                 <TextInput style={styles.input} 
                     placeholder="Username or email"
                     placeholderTextColor="rgba(149, 165, 166,0.7)"
+                    returnKeyType="next"
+                    keyboardType='email-address'
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    
+                    onSubmitEditing={() => this.passwordInput.focus()}
                 />
                 <TextInput style={styles.input} 
+                    ref={(input) => this.passwordInput = input}
                     placeholder="Password"
                     placeholderTextColor="rgba(149, 165, 166,0.7)"
                     secureTextEntry
+                    returnKeyType="go"
                 />
                 <TouchableOpacity style={styles.button}>
                     <Text style={styles.buttonText}> LOGIN </Text>
